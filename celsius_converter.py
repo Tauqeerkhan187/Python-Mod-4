7aba0857a5789f01ab9d6d9444834ffb6ba17daa
TauqeerKhan187 <tauqeerkhan1888@gmail.com>
2025-10-23 09:47:27 +0300

    celsius converter for python

---
+++ celsius_converter.py
@@ -0,0 +1,7 @@
+import math
+# Get user input
+c = float(input("Enter temperature in Celsius: "))
+# formula to convert Celsius to Fahrenheit
+f = (c * 9/5) + 32
+# Display the result
+print("Temperature in Fahrenheit:", f)
